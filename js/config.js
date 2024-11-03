33430c1129a9bce2283d69193b1eb7d1ea1081e2
moliver0621 <152496537+moliver0621@users.noreply.github.com>
2024-11-02 22:54:49 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_9ouZ6kjIu', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5mi13jf1mb71283neeko9cenbp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
